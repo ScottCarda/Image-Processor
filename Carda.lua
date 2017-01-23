529dbef0d48b666ca796dec60f8795cb6cdd6179
Scott Carda <ScottCarda2012@hotmail.com>
2017-01-22 17:33:12 -0700

    Added Brighten, Gamma, and Discrete Pseudocolor

--- Carda.lua
+++ Carda.lua
@@ -1,30 +1,145 @@
-require "ip"
+require("ip")
+local il = require "il"
 
 local funcs = {}
 
+local function in_range( val )
+  
+  if val > 255 then
+    return 255
+  elseif val < 0 then
+    return 0
+  else
+    return val
+  end
+  
+end
+
 function funcs.grayscaleRGB( img )
-  print( "Unimplemented" )
+  local val
+  local pix
+  
+  for row = 0, img.height-1 do
+    for col = 0, img.width-1 do
+      
+      pix = img:at( row, col )
+      
+      val = pix.r * 30
+      val = val + pix.g * 59
+      val = val + pix.b * 11
+      val = math.floor( val / 100 )
+      
+      for chan = 0, 2 do
+        pix.rgb[chan] = val
+      end
+      
+    end
+  end
   return img
 end
 
-function funcs.threshold( img )
-  print( "Unimplemented" )
+function funcs.threshold( img, threshold )
+  local val
+  local pix
+  
+  -- convert image from RGB to YIQ
+  il.RGB2YIQ( img )
+  
+  for row = 0, img.height-1 do
+    for col = 0, img.width-1 do
+      
+      pix = img:at( row, col )
+      
+      if pix.y <= threshold then
+        val = 0
+      else
+        val = 255
+      end
+      
+      for chan = 0, 2 do
+        pix.rgb[chan] = val
+      end
+      
+    end
+  end
+  
   return img
 end
 
-function funcs.brighten( img )
-  print( "Unimplemented" )
+function funcs.brighten( img, val )
+  local pix
+  
+  for row = 0, img.height-1 do
+    for col = 0, img.width-1 do
+      
+      pix = img:at( row, col )
+      
+      for chan = 0, 2 do
+        pix.rgb[chan] = in_range( pix.rgb[chan] + val )
+      end
+      
+    end
+  end
+  
   return img
 end
 
---{"Gamma", funcs.gamma, {{name = "gamma", type = "string", default = "1.0"}}},
-function funcs.gamma( img )
-  print( "Unimplemented" )
+function funcs.gamma( img, gamma )
+  local c = 255
+  local pix
+  
+  for row = 0, img.height-1 do
+    for col = 0, img.width-1 do
+      
+      pix = img:at( row, col )
+      
+      for chan = 0, 2 do
+        pix.rgb[chan] = in_range( c * ( pix.rgb[chan] / c ) ^ gamma )
+      end
+      
+    end
+  end
+  
   return img
 end
 
 function funcs.disc_pseudocolor( img )
-  print( "Unimplemented" )
+  local pix
+  local min = 0
+  local max = 256
+  local num_colors = 8
+  
+  local divisor = ( max - min ) / num_colors
+  
+  local color_table =
+    {
+      {000,000,000},
+      {128,000,128},
+      {000,000,255},
+      {000,255,000},
+      {255,255,000},
+      {255,128,000},
+      {255,000,000},
+      {255,255,255}
+    }
+  
+  -- convert image from RGB to YIQ
+  il.RGB2YIQ( img )
+  
+  for row = 0, img.height-1 do
+    for col = 0, img.width-1 do
+      
+      pix = img:at( row, col )
+      
+      color = math.floor( ( pix.y - min ) / divisor )
+      
+      for chan = 0, 2 do
+        pix.rgb[chan] = color_table[color+1][chan+1]
+      end
+      
+    end
+  end
+  
   return img
 end
 
--- menu.lua
+++ menu.lua
@@ -9,12 +9,13 @@ imageMenu("Point processes",
     {"Negate", funcs.negate},
     --{"Posterize", funcs.posterize, {{name = "levels", type = "number", displaytype = "spin", default = 8, min = 2, max = 64}}},
     {"Posterize", funcs.posterize},
-    {"Brighten", funcs.brighten},
+    {"Brighten", funcs.brighten, {{name = "Value", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}},
     --{"Gamma", funcs.gamma, {{name = "gamma", type = "string", default = "1.0"}}},
-    {"Gamma", funcs.gamma},
+    {"Gamma", funcs.gamma, {{name = "Gamma", type = "string", default = "1.0"}}},
     {"Log", funcs.logscale},
     {"Cont Pseudocolor", funcs.cont_pseudocolor},
     {"Disc Pseudocolor", funcs.disc_pseudocolor},
+    {"His Pseudocolor", il.pseudocolor2},
     --{"Bitplane Slice", funcs.slice, {{name = "plane", type = "number", displaytype = "spin", default = 7, min = 0, max = 7}}}
     {"Bitplane Slice", funcs.slice}
   }
@@ -35,8 +36,8 @@ imageMenu("Histogram processes",
 
 imageMenu("Misc",
   {
-    --{"Binary Threshold", funcs.threshold, {{name = "threshold", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}}
-    {"Binary Threshold", funcs.threshold}
+    {"Binary Threshold", funcs.threshold, {{name = "Threshold", type = "number", displaytype = "slider", default = 128, min = 0, max = 255}}}
+    --{"Binary Threshold", funcs.threshold}
   }
 )
 
