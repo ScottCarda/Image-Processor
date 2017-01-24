cf251c0fbd4b285b451b535cdf4a2695b84e1d76
Chris Smith <chris.sdsmt@gmail.com>
2017-01-24 13:51:47 -0700

    removed third contrast from menu

--- menu.lua
+++ menu.lua
@@ -27,7 +27,7 @@ imageMenu("Histogram processes",
     {"Contrast Specify", funcs.stretchSpecify,
       {{name = "lp", type = "number", displaytype = "spin", default = 1, min = 0, max = 100},
        {name = "rp", type = "number", displaytype = "spin", default = 99, min = 0, max = 100}}},
-    {"Contrast Specify", funcs.stretchSpecify},
+    --{"Contrast Specify", funcs.stretchSpecify},
     {"Histogram Equalize RGB", funcs.equalizeRGB},
     --{"Histogram Equalize Clip", funcs.equalizeClip, {{name = "clip %", type = "number", displaytype = "textbox", default = "1.0"}}}
     {"Histogram Equalize Clip", funcs.equalizeClip}
