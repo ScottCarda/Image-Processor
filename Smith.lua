eada2a60505ad592236afb386cfae216db534318
Chris Smith <chris.sdsmt@gmail.com>
2017-01-24 11:45:24 -0700

    Posterize code

--- Smith.lua
+++ Smith.lua
@@ -3,8 +3,16 @@ require "ip"
 local funcs = {}
 
 --{"Posterize", funcs.posterize, {{name = "levels", type = "number", displaytype = "spin", default = 8, min = 2, max = 64}}},
-function funcs.posterize( img )
-  print( "Unimplemented" )
+function funcs.posterize( img, levels )
+  local interval = 256/4
+  local quanta = math.floor(255/(levels-1))
+  for row = 0, img.height-1 do
+    for col = 0, img.width-1 do
+      for chan = 0, 2 do
+        img:at(row, col ).rgb[chan] = math.floor(img:at( row, col ).rgb[chan]/interval) * quanta
+      end
+    end
+  end
   return img
 end
 
