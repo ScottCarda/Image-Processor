8c899713213944161dae411c778c615bdf96f4a0
CsmithSD <chris.sdsmt@gmail.com>
2017-02-08 10:28:58 -0700

    Added authors and formatted

--- HelperFuncs.lua
+++ HelperFuncs.lua
@@ -20,26 +20,28 @@ local helpers = {}
   |   Takes an image and a channel specifier. Loops through the
   |   image finding the minimum and maximum intensities for
   |   that channel of the image. Returns the minimum and maximum found.
+  |
+  |     Authors: Christopher Smith, Scott Carda
 --]]
 function helpers.get_minmax_intensities( img, chan )
   local min = 256 -- the minimum intensity found
   local max = -1 -- the maximum intensity found
   local pix -- a pixel
-  
+
   -- loop through the image, setting min and max
   for row, col in img:pixels() do
-    
+
     pix = img:at( row, col )
-    
+
     if min > pix.rgb[chan] then
-        min = pix.rgb[chan]
+      min = pix.rgb[chan]
     end
     if max < pix.rgb[chan] then
       max = pix.rgb[chan]
     end
-    
+
   end
-  
+
   return min, max
 end
 
@@ -48,9 +50,11 @@ end
   |   Takes a value and return the value if it is in the range 0 to 255.
   |   If it is not in the range, it returns the range's endpoint that is
   |   the closest.
+  |
+  |     Author: Scott Carda
 --]]
 function helpers.in_range( val )  
-  
+
   if val > 255 then
     return 255
   elseif val < 0 then
@@ -58,48 +62,54 @@ function helpers.in_range( val )
   else
     return val
   end
-  
+
 end
 
 --[[    get_hist
   |
   |   Takes an image and a channel specifier. Creates and returns a
   |   table representation of the histogram for that channel of the image.
+  |
+  |     Authors: Scott Carda
 --]]
 function helpers.get_hist( img, chan )
   local pix
   local hist = {}
-  
+
   -- initialize the histogram
   for i = 0, 255 do
     hist[i] = 0
   end
-  
+
   for row, col in img:pixels() do
-    
+
     pix = img:at( row, col )
-    
+
     hist[pix.rgb[chan]] = hist[pix.rgb[chan]] + 1
-    
+
   end
-  
+
   return hist
 end
 
 --[[    round
-  |
+  |     
   |   Takes a value and return the value rounded to the nearest integer.
+  |
+  |   Authors: Scott Carda
 --]]
 function helpers.round( val )
   return math.floor( val + 0.5 )
 end
 --[[    get_percent_location
-  ]
+  ]     
   |   Takes in the histogram and total number of pixels in the image. It then 
-  ]   finds bin the is either greater than or equal to the percentage of pixels 
-  ]   the user is looking for. The value of start dictates starting from the bin 
-  ]   0 or bin 255 to find the location from the end that sums up to or greater 
-  ]   than the percentage of pixels looking for.
+  |   finds bin the is either greater than or equal to the percentage of pixels 
+  |   the user is looking for. The value of start dictates starting from the bin 
+  |   0 or bin 255 to find the location from the end that sums up to or greater 
+  |   than the percentage of pixels looking for.
+  |
+  |   Authors: Christopher Smith
 --]]
 --
 function helpers.get_percent_location( hist, num_pixels, percent, start )
@@ -123,10 +133,12 @@ end
 --[[    clip_hist
   |
   |   Takes in a histogram and the maximum number of pixels per bin sets any bin 
-  ]   in the histogram greater than that number to that number. the cut pixels 
-  ]   are are kept track of and the bins cut from so the pixels can later be 
-  ]   redistributed throughout the histogram with an emphasis of placing extra 
-  ]   pixels in the largest bins cut from
+  |   in the histogram greater than that number to that number. the cut pixels 
+  |   are are kept track of and the bins cut from so the pixels can later be 
+  |   redistributed throughout the histogram with an emphasis of placing extra 
+  |   pixels in the largest bins cut from
+  |
+  |     Authors: Christopher Smith
 --]]
 function helpers.clip_hist( hist, max_pixels)
   local cut_pixels = 0
@@ -141,14 +153,16 @@ function helpers.clip_hist( hist, max_pixels)
     end
   end
   return hist, cut_pixels
-   --redistribute clipped pixels over histogram with leftover pixels placed in the largest bins
+  --redistribute clipped pixels over histogram with leftover pixels placed in the largest bins
   --return helpers.distribute_cut_pixels(hist, cut_pixels, largest_bins, k)
 end
 --[[    distribute_cut_pixels
   |
   |   Takes in a histogram and the cut pixels and largest bins cut from. It
-  ]   redistributs the cut pixels throughout the histogram with an emphasis 
-  ]   of placing extra pixels in the largest bins cut from
+  |   redistributs the cut pixels throughout the histogram with an emphasis 
+  |   of placing extra pixels in the largest bins cut from
+  |
+  |     Authors: Christopher Smith
 --]]
 function helpers.distribute_cut_pixels( hist, cut_pixels, largest_bins, size)
   local mod = cut_pixels % 256
@@ -158,7 +172,7 @@ function helpers.distribute_cut_pixels( hist, cut_pixels, largest_bins, size)
   end
   if mod ~= 0 then
     local k = 0
-    for i = 0, mod do
+    for _ = 0, mod do
       hist[ largest_bins[k] ] = hist[ largest_bins[k] ] + 1
       k = (k + 1) % size
     end
@@ -166,7 +180,10 @@ function helpers.distribute_cut_pixels( hist, cut_pixels, largest_bins, size)
   return hist
 end
 --[[    convert_img
+  |
   |   Returns a converted img based on the color model a user selects
+  |
+  |   Authors: Christopher Smith  
 --]]
 function helpers.convert_img( img, model)
   --convert image based on model selected
@@ -179,8 +196,11 @@ function helpers.convert_img( img, model)
   end
   return img
 end
---[[    convert_img
+--[[    convert_2rgb
+  |  
   |   Returns an img in RGB based on the color model a user selects
+  |
+  |   Authors: Christopher Smith
 --]]
 function helpers.convert_2rgb(img, model)
   --convert image back to RGB
@@ -194,8 +214,11 @@ function helpers.convert_2rgb(img, model)
   return img
 end
 --[[    use_lut
+  |     
   |   Applies the look up table created for a specific image process to the image pixels
-  ]   n_chans is 2 or 0 meaning the user or process is modifying rgb or intensity values
+  |   n_chans is 2 or 0 meaning the user or process is modifying rgb or intensity values
+  |
+  |   Authors: Christopher Smith
 --]]
 function helpers.use_lut( img, lut, n_chans)
   for row, col in img:pixels() do
@@ -207,10 +230,13 @@ function helpers.use_lut( img, lut, n_chans)
   return img
 end
 --[[    choose
+  |
   |   Simple helper function to clean up if then else statements
+  |
+  |   Authors: Christopher Smith
 --]]
 function helpers.choose(cond,a,b)
-    if cond then return a else return b end
+  if cond then return a else return b end
 end
 
 
