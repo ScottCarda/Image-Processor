9441390c3c76a25627d36e56a31fe429f3647cb9
CsmithSD <chris.sdsmt@gmail.com>
2017-03-09 15:16:28 -0700

    Edge Detection file

---
+++ neighborhood/Edge_Detection.lua
@@ -0,0 +1,12 @@
+--[[
+  |                                  Main.lua                                  |
+  |                                                                            |
+  |                                                                            |
+  |   This file contains the fucntion definitions for the edge detection       |
+  |   menu items.                                                               |
+  |                                                                            |
+  |   Authors:                                                                 |
+  |     Scott Carda,                                                           |
+  |     Christopher Smith                                                      |
+  |                                                                            |
+--]]
