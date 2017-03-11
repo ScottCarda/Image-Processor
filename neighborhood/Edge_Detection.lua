650d96668bf89035849141e0df2d1a053c9ca56a
Scott Carda <ScottCarda2012@hotmail.com>
2017-03-11 15:54:05 -0700

    Commented

--- neighborhood/Edge_Detection.lua
+++ neighborhood/Edge_Detection.lua
@@ -17,7 +17,18 @@ local helpers = require ( "Helper_Funcs" )
 
 local funcs = {}
 
-function funcs.new_sobel( img )
+--[[    sobel
+  |
+  |   Takes an image and calculates the smoothed sobel magnitude and direction.
+  |   This is an edge detection operation that uses the x and y partial
+  |   derivatives to find local extrema, which represent edges. The partial derivatives
+  |   are approximated using separated filters to increase the efficiency of the operation.
+  |
+  |     Authors:
+  |       Scott Carda,
+  |       Christopher Smith
+--]]
+function funcs.sobel( img )
   
   -- make a copy of the image to return
   local cpy_img = img:clone()
@@ -106,116 +117,6 @@ function funcs.new_sobel( img )
   
   return cpy_img, mag_img, dir_img
   
-end
-
---[[    part_deriv
-  |
-  |   Takes an image and a position in the image. Approximates the partial
-  |   derivatives for x and y directions. Performs a preprocessing smooth operation.
-  |
-  |     Author: Scott Carda
---]]
-local function part_deriv( img, row, col )
-  
-  local part_y = 0 -- the partial derivative for y
-  local part_x = 0 -- the partial derivative for x
-  local val -- value of a particular pixel's intensity
-  local x, y -- coordinates for a pixel
-  
-  -- the y direciton difference filter, with smoothing 
-  local y_diff = {
-    { 1, 2, 1},
-    { 0, 0, 0},
-    {-1,-2,-1}
-  }
-  
-  -- the x direciton difference filter, with smoothing 
-  local x_diff = {
-    {-1, 0, 1},
-    {-2, 0, 2},
-    {-1, 0, 1}
-  }
-  
-  -- neighborhood loop
-  for i = 1, 3 do
-    y = helpers.reflection( row+i-2, 0, img.height )
-    for j = 1, 3 do
-      -- skip central element of the filters
-      if i ~= 2 or j ~= 2 then
-        x = helpers.reflection( col+j-2, 0, img.width )
-        
-        -- calculate part_x, part_y
-        val = img:at( y, x ).r
-        part_y = part_y + val * y_diff[i][j]
-        part_x = part_x + val * x_diff[i][j]
-        
-      end
-    end
-  end
-  
-  return part_y, part_x
-end
-
---[[    sobel
-  |
-  |   Takes an image and calculates the smoothed sobel magnitude and direction.
-  |   This is an edge detection operation that uses the x and y
-  |   partial derivatives to find local extrema, which represent edges.
-  |
-  |     Author: Scott Carda
---]]
-function funcs.sobel( img )
-
-  -- make a copy of the image to return
-  local cpy_img = img:clone()
-
-  -- convert image from RGB to YIQ
-  il.RGB2YIQ( img )
-  
-  local dir_img = img:clone() -- sobel direction image
-  local mag_img = img:clone() -- sobel magnitude image
-  local dir_pix -- a pixel from the dir_img
-  local mag_pix -- a pixel from the mag_img
-  
-  local part_y -- the partial derivative for y
-  local part_x -- the partial derivative for x
-  local val -- unclipped calculated value of pixel sobel magnitude
-  
-  for row, col in img:pixels() do
-    dir_pix = dir_img:at( row, col )
-    mag_pix = mag_img:at( row, col )
-    
-    -- get the sobel partial x and partial y
-    part_y, part_x = part_deriv( img, row, col )
-    
-    -- calculate the magnitude
-    val = math.floor( math.sqrt( part_x*part_x + part_y*part_y ) )
-    mag_pix.r = helpers.in_range( val )
-    
-    -- calculate the direction using arctangent
-    val = math.atan2( part_y, part_x )
-    -- adjust for negative values
-    if val < 0 then
-      val = val + 2 * math.pi
-    end
-    
-    -- map from radians to pixel intensities
-    val = math.floor( val / ( 2 * math.pi ) * 256 )
-    dir_pix.r = helpers.in_range( val )
-    
-    -- remove the color
-    mag_pix.g = 128
-    mag_pix.b = 128
-    dir_pix.g = 128
-    dir_pix.b = 128
-  end
-  
-  -- convert image from YIQ to RGB
-  il.YIQ2RGB( dir_img )
-  il.YIQ2RGB( mag_img )
-  
-  return cpy_img, mag_img, dir_img
-
 end
 
 --[[    kirsch
